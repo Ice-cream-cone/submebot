8e6a926bcebe9b583b01cf4fa78c589aa73d22ae
Charlie <achanont@gmail.com>
2020-01-03 16:43:03 -0600

    first init

---
+++ bot.js
@@ -0,0 +1,10 @@
+const Snoowrap = require('snoowrap');
+const { CommentStream } = require('snoostorm');
+
+const client = new Snoowrap({
+  userAgent: 'my-node-js-bot',
+  clientId: 'qR6rJnQ7sEJZDw',
+  clientSecret: 'OCoo9pYnlC2K6fxQQxbcIPQ5MA4',
+  username: 'myusernamebutactuallybot',
+  password: 'mypasswordbutactuallybot'
+});
